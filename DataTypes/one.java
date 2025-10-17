9cebe1e400b34d618aa83679553e53ef75e666f7
vinaykumar2004vinay <vinaykumardasari2004@gmail.com>
2025-10-17 09:55:21 +0530

    JAVA

---
+++ DataTypes/one.java
@@ -0,0 +1,29 @@
+class one 
+{
+    public static void main(String args[])
+    {
+        Boolean aa=true;
+        Boolean bb=false;
+        char a = 'G';
+        int i = 89; //-2,147,483,648 to 2,147,483,647
+        byte b = 4; //Stores whole numbers from -128 to 127
+        short s = 56; //-32,768 to 32,767
+        double d = 4.355453532; //15 decimal digits
+        //for float use 'f' as suffix as standard
+        float f = 4.7333434f; //6 to 7 decimal digits
+        //need to hold big range of numbers then we need
+        long l = 12121;
+        // System.out.println("char: " + a);
+        // System.out.println("integer: " + i);
+        // System.out.println("byte: " + b);
+        // System.out.println("short: " + s);
+        // System.out.println("float: " + f);
+        // System.out.println("double: " + d);
+        // System.out.println("long: " + l);
+        // System.out.println(Byte.SIZE);
+        System.out.println(Byte.MIN_VALUE);
+        System.out.println(Byte.MAX_VALUE);
+        // System.out.println("Bool: " + aa);
+        // System.out.println("Bool: " + bb);
+    }
+}

f47495947eb06857f8c5f2837e3f6ff1e3ce5003
vinaykumar2004vinay <vinaykumardasari2004@gmail.com>
2025-10-21 16:53:10 +0530

    JAVA

---
+++ Functions/one.java
@@ -0,0 +1,19 @@
+class one
+{
+    public static void message()
+    {
+        System.out.println("Hello My Name is Vinay Kumar");
+    }
+
+    public static void sum(int a,int b)
+    {
+        int c;
+        c = a+b;
+        System.out.println("sum of :  " + a + " and : " + b + " is :  " + c);
+    }
+    public static void main(String a[])
+    {
+        message();
+        sum(10,20);
+    }
+}
---
+++ Functions/two.java
@@ -0,0 +1,13 @@
+class two
+{
+    public static void sum(int a,int b)
+    {
+        int c;
+        c = a + b;
+        System.out.println("c="+(a+b));
+    }
+    public static void main(String args[])
+    {
+        sum(10,20);
+    }
+}

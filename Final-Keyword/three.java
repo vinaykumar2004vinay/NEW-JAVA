806db0659a2b3bd2df57767c6c339458fa3416f3
vinaykumar2004vinay <vinaykumardasari2004@gmail.com>
2025-10-27 11:00:39 +0530

    JAVA

---
+++ Final-Keyword/three.java
@@ -0,0 +1,9 @@
+public class three
+{
+    public static void main(String args[])
+    {
+        final int myvalue = 100;
+        myvalue = 200;                   //cannot assign a value to final variable myvalue
+        System.out.println(myvalue);
+    }
+}
